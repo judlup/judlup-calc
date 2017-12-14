b2d69df1e6a35bdbcbdd004ef8aba62aeacf8500
JudLup Luna <judlup@gmail.com>
2017-12-14 03:22:09 -0500

    judlup-cal

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports =
+{  
+  printMsg : function() {
+    console.log("Hello to Judlup-Calc");
+    return "Hello to Judlup-Calc";
+  },
+  add : function(a , b) {
+  	return a + b;
+  },
+  subs : function(a , b) {
+  	return a - b;
+  },
+  mult : function(a , b) {
+  	return a * b;
+  },
+  div : function(a , b) {
+  	return a / b;
+  },
+  mod : function(a , b) {
+  	return a % b;
+  }
+}
+
