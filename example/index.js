859bdf03935ad9b2015fbc4d2b3a69b64927b18a
JudLup Luna <judlup@gmail.com>
2017-12-14 03:25:51 -0500

    judlup-calc

---
+++ example/index.js
@@ -0,0 +1,20 @@
+//var calc = require("judlup-calc");
+var calc = require("../index.js");
+
+calc.printMsg();
+
+//Add
+var add = calc.add(8,5);
+console.log("Add result is : ",add);
+
+//Substract
+var subs = calc.subs(17,4);
+console.log("Substract result is : ",subs);
+
+//Multiply
+var mult = calc.mult(2,6.5);
+console.log("Multiply result is : ",mult);
+
+//Divide
+var div = calc.div(26,2);
+console.log("Divide result is : ",div);
